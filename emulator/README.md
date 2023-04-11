dc931e0db68071ab6fedfc3f0f408f46b73469b9
lley154 <lawrence.ley@contextsolutions.ca>
2023-04-10 20:02:44 -0400

    - added emulator to the helios example repo - can be used as a starter template for unit and integration test suites

---
+++ emulator/README.md
@@ -0,0 +1,42 @@
+## This is an example of using the Helios emulator
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm install
+npm test
+
+
+## Output to console will be like the following:
+```
+~/src/helios-examples/emulator$ npm test
+
+> emulator@1.0.0 test
+> node ./src/mint.mjs
+
+************ PRE-TEST ************
+wallet txId 0000000000000000000000000000000000000000000000000000000000000000#0
+value { lovelace: '10000000', assets: {} }
+
+************ EXECUTE SMART CONTRACT ************
+TT1: true
+TT2: true
+TT1: true
+TT2: true
+
+************ SUBMIT TX ************
+TxId 500fbde8956a875dcf1cf89cfd7bf8fd199d5019da848ba3df9683f46ef08913
+
+************ POST-TEST ************
+wallet txId 500fbde8956a875dcf1cf89cfd7bf8fd199d5019da848ba3df9683f46ef08913#0
+value {
+  lovelace: '2000000',
+  assets: {
+    '2e731a86d95b79d54da0e95f248bd7392891ee6972f771a1271575bb': { '54687265616420546f6b656e': '1' }
+  }
+}
+wallet txId 500fbde8956a875dcf1cf89cfd7bf8fd199d5019da848ba3df9683f46ef08913#1
+value { lovelace: '7757110', assets: {} }
+```
---
+++ emulator/src/mint.mjs
@@ -0,0 +1,111 @@
+import { promises as fs } from 'fs';
+import {
+  Assets, 
+  ConstrData, 
+  NetworkEmulator,
+  NetworkParams,
+  Program, 
+  Value, 
+  textToBytes,
+  TxOutput,
+  Tx, 
+} from "./helios.mjs";
+
+/**
+* Main calling function via the command line 
+* Usage: npm test
+*/
+const main = async () => {
+
+  // Set the Helios compiler optimizer flag
+  let optimize = false;
+  const minAda = BigInt(2000000);  // minimum lovelace needed to send an NFT
+
+  try {
+      // Create an Instance of NetworkEmulator
+      const network = new NetworkEmulator();
+
+      // Create a Wallet - we add 10ADA to start
+      const alice = network.createWallet(BigInt(10000000));
+
+      // Now lets tick the network on 10 slots,
+      // this will allow the UTxOs to be created from Genisis
+      network.tick(BigInt(10));
+
+      // Now we are able to get the UTxOs in Alices Wallet
+      // Note we provide the Address here, this is useful 
+      // for getting the UTxOs from a script etc (later!)
+      const utxos = await network.getUtxos(alice.address);
+      console.log("************ PRE-TEST ************");
+
+      for (const utxo of utxos) {
+        console.log("wallet txId", utxo.txId.hex + "#" + utxo.utxoIdx);
+        console.log("value", utxo.value.dump());
+      }
+
+      // NFT minting script
+      const nftScript = await fs.readFile('./src/nft.hl', 'utf8');
+      const nftProgram = Program.new(nftScript);
+      nftProgram.parameters = {["TX_ID"] : utxos[0].txId.hex};
+      const nftMPH = nftProgram.compile(optimize).mintingPolicyHash;
+
+      // Start building the transaction
+      const tx = new Tx();
+
+      // Add the UTXO as inputs
+      tx.addInputs(utxos);
+
+      // Add the script as a witness to the transaction
+      tx.attachScript(nftProgram.compile(optimize));
+
+      // Create an empty Redeemer because we must always send a Redeemer with
+      // a plutus script transaction even if we don't actually use it.
+      const nftRedeemer = new ConstrData(0, []);
+      const token = [[textToBytes("Thread Token"), BigInt(1)]];
+      
+      // Add the mint to the tx
+      tx.mintTokens(
+          nftMPH,
+          token,
+          nftRedeemer
+      )
+
+      // Attach the output with the minted nft to the destination address
+      tx.addOutput(new TxOutput(
+          alice.address,
+          new Value(minAda, new Assets([[nftMPH, token]]))
+        ));
+
+        // Network Parameters
+      const networkParamsFile = await fs.readFile('./src/preprod.json', 'utf8');
+      const networkParams = new NetworkParams(JSON.parse(networkParamsFile.toString()));
+
+      console.log("");
+      console.log("************ EXECUTE SMART CONTRACT ************");
+      await tx.finalize(networkParams, alice.address, utxos);
+
+      console.log("");
+      console.log("************ SUBMIT TX ************");
+      // Submit Tx to the network
+      const txId = await network.submitTx(tx);
+      console.log("TxId", txId.dump());
+
+      // Tick the network on 10 more slots,
+      network.tick(BigInt(10));
+
+      const utxosFinal = await network.getUtxos(alice.address);
+      console.log("");
+      console.log("************ POST-TEST ************");
+      for (const utxo of utxosFinal) {
+        console.log("wallet txId", utxo.txId.hex + "#" + utxo.utxoIdx);
+        console.log("value", utxo.value.dump());
+      }
+      
+  } catch (err) {
+      console.error(err);
+  }
+}
+
+main();
+
+
